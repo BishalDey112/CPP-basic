aa78fc7c4b3408f3c6614ebb772d4d453af7caed
Bishal Dey <bishaldey.in@gmail.com>
2026-08-16 20:34:53 +0530

    strings

---
+++ Strings/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //declare
+    string name;
+    name.push_back('a');
+    name.push_back('b');
+    name.push_back('c');
+    name.push_back('d');
+    cout<<name<<endl;
+
+    //initialization
+    string n
+
+    return 0;
+}
